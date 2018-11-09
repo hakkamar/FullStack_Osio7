130903be340c985ebce3f591ad51283c12dba307
hakkamar <hakkis68@gmail.com>
2018-11-09 14:15:30 +0200

    initial

---
+++ frontend/src/components/Togglable.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Togglable from './Togglable'
+
+describe('<Togglable />', () => {
+  let togglableComponent
+
+  beforeEach(() => {
+    togglableComponent = shallow(
+      <Togglable buttonLabel="show...">
+        <div className="testDiv" />
+      </Togglable>
+    )
+  })
+
+  it('renders its children', () => {
+    expect(togglableComponent.contains(<div className="testDiv" />)).toEqual(true)
+  })
+
+  it('at start the children are not displayed', () => {
+    const div = togglableComponent.find('.togglableContent')
+    expect(div.getElement().props.style).toEqual({ display: 'none' })
+  })
+
+  it('after clicking the button, children are displayed', () => {
+    const button = togglableComponent.find('button')
+
+    button.at(0).simulate('click')
+    const div = togglableComponent.find('.togglableContent')
+    expect(div.getElement().props.style).toEqual({ display: '' })
+  })
+})
